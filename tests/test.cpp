78cf903169e57d9602299eb94a46b7022638a6b8
Evan-pocan <vanya.kek0@gmail.com>
2022-03-31 14:27:48 +0300

    Creating

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,10 @@
 //
 // Tests
 //
-
-#include <iostream>
-#include <gtest/gtest.h>
-#include <sys/mman.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 #include <unistd.h>
+#include "gtest/gtest.h"
 
 extern "C" {
 #include "func_calculation.h"
@@ -23,7 +21,7 @@ TEST(DIRECT_CALC_TEST, SUCCESS_DIRECT_SUM) {
     int64_t test_result = 0;
     int *test_arr = read_file_to_memory("data_test.bin", test_arr_size);
     direct_sum_calculation(&test_result, test_arr, test_arr_size);
-    EXPECT_TRUE(test_result == 1000);
+    EXPECT_EQ(test_result, 1000);
 }
 
 TEST(PARALLEL_CALC_TEST, SUCCESS_PARALLEL_SUM) {
@@ -31,28 +29,35 @@ TEST(PARALLEL_CALC_TEST, SUCCESS_PARALLEL_SUM) {
     int *test_arr = read_file_to_memory("data_test.bin", test_arr_size);
     int init_pid = getpid();
     size_t num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-    auto *test_result = static_cast<int64_t *>(mmap(nullptr, sizeof(int),
-                                                    PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    auto *test_result = static_cast<int64_t *>
+            (mmap(nullptr, sizeof(int), PROT_READ | PROT_WRITE,
+                  MAP_SHARED | MAP_ANONYMOUS, -1, 0));
     *test_result = 0;
     parrallel_sum_calculation(test_arr, num_cores, test_result, test_arr_size);
     if (getpid() != init_pid) {
-        while (wait(nullptr) > 0);
+        while (wait(nullptr) > 0) {}
         exit(0);
     }
-    while (wait(nullptr) > 0);
-    EXPECT_TRUE(*test_result == 1000);
+    while (wait(nullptr) > 0) {}
+    EXPECT_EQ(*test_result, 1000);
 }
 
 TEST(MMAP_CLEAR_TEST, SUCCESS_CLEAR) {
     size_t test_arr_size = 1000;
     int *test_arr = read_file_to_memory("data_test.bin", test_arr_size);
     int flag = clear_mem(test_arr, test_arr_size);
-    EXPECT_TRUE(flag == 0);
+    EXPECT_EQ(flag, 0);
+}
+
+TEST(DIRECT_TEST, SUCCESS_DIRECT) {
+    size_t test_arr_size = 1000;
+    int *test_arr = read_file_to_memory("data_test.bin", test_arr_size);
+    int64_t test_result = calculate_sum(test_arr, test_arr_size);
+    EXPECT_EQ(test_result, 1000);
 }
 
 
 int main(int argc, char *argv[]) {
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
